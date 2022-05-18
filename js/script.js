6e7bfb6710ebf83ae11361c6f3a745b900bad927
Vlad <example@gmail.com>
2022-05-19 00:06:48 +0700

    teach js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 "use strict";
+const numberOfFilms = +prompt("Сколько фильмов ты глянул?", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const category = 'toys';
-const user = 'Ivan';
+let index = 0;
+while (index <= 1) {
+    let question = prompt("Последний фильм?","");
+    let goal = +prompt("Оценка?", "");
+    personalMovieDB.movies[question] = goal;
+    index++;
+    }
 
-console.log(`https://somurl.com/${category}/5`);
-alert(`Hello , ${user}`);
+console.log(personalMovieDB);
